f7f54430a005ae68cc84f59666c8d40f26ff213b
Kaniz Fatema <kaniz.ifte@gmail.com>
2020-08-16 23:29:12 -0700

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    @yield('meta')
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet">
+
+    @yield('css')
+</head>
+
+<body>
+
+    <div id="particles-js">
+    </div>
+    <div id="app">
+        <nav class="navbar navbar-expand-md">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img class="brand-logo" src="{{asset('assets\images\logo.png')}}">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon">
+                        <img src="{{asset('assets\images\menu.png')}}" alt="">
+                    </span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                        <li class="nav-item">
+                            <a class="nav-link btn" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link btn" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+                        @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" style="color:#fff" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+
+        <footer>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+
+                        <div class="footer-left">
+                            <a href="#">
+
+                                <img src="{{asset('assets\images\yps_logo.png')}}" class="yps_logo">
+
+                            </a>
+
+
+                        </div>
+
+                        <div class="footer-right">
+                            <a href="#" class="btn facebook_share_button">
+
+                                <img src="{{asset('assets\images\facebook_share.png')}}" class="facebook_share"> <span>SHARE ON FACEBOOK</span>
+
+                            </a>
+
+
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </footer>
+    </div>
+
+
+    <!-- Scripts -->
+
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="{{ asset('assets/js/particles.min.js') }}" ></script>
+    <script src="{{ asset('assets/js/script.js') }}" ></script>
+    @yield('js')
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+    body {
+        font-size: 100%;
+        font-family: monospace, sans-serif;
+        width: 100%;
+    }
+
+
+    /* Beginning of Navbar CSS */
+    body {
+        background-color: #2F3242;
+    }
+
+    svg {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-top: -250px;
+        margin-left: -400px;
+    }
+
+    .message-box {
+        height: 200px;
+        width: 380px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-top: -100px;
+        margin-left: 50px;
+        color: #FFF;
+        font-family: Roboto;
+        font-weight: 300;
+    }
+
+    .message-box h1 {
+        font-size: 60px;
+        line-height: 46px;
+        margin-bottom: 40px;
+    }
+
+    .buttons-con .action-link-wrap {
+        margin-top: 40px;
+    }
+
+    .buttons-con .action-link-wrap a {
+        background: #68c950;
+        padding: 8px 25px;
+        border-radius: 4px;
+        color: #FFF;
+        font-weight: bold;
+        font-size: 14px;
+        transition: all 0.3s linear;
+        cursor: pointer;
+        text-decoration: none;
+        margin-right: 10px
+    }
+
+    .buttons-con .action-link-wrap a:hover {
+        background: #5A5C6C;
+        color: #fff;
+    }
+
+    #Polygon-1,
+    #Polygon-2,
+    #Polygon-3,
+    #Polygon-4,
+    #Polygon-4,
+    #Polygon-5 {
+        animation: float 1s infinite ease-in-out alternate;
+    }
+
+    #Polygon-2 {
+        animation-delay: .2s;
+    }
+
+    #Polygon-3 {
+        animation-delay: .4s;
+    }
+
+    #Polygon-4 {
+        animation-delay: .6s;
+    }
+
+    #Polygon-5 {
+        animation-delay: .8s;
+    }
+
+    @keyframes float {
+        100% {
+            transform: translateY(20px);
+        }
+    }
+
+    @media (max-width: 450px) {
+        svg {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin-top: -250px;
+            margin-left: -190px;
+        }
+
+        .message-box {
+            top: 50%;
+            left: 50%;
+            margin-top: -100px;
+            margin-left: -190px;
+            text-align: center;
+        }
+    }
+
+    .navbar {
+        position: fixed;
+        display: inline-block;
+        top: 0;
+        left: 0;
+        right: 0;
+        z-index: 9999;
+        height: 60px;
+        width: 100%;
+        background: #bbb;
+        padding: 10px 20px;
+        float: none;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        float: right;
+        margin: 0px 10px;
+    }
+
+
+    /* End of Navbar CSS */
+
+
+    /* Welcome Page */
+
+    #welcome-page {
+        background-image: url(http://www.bartsalle.nl/wp-content/uploads/2013/12/background-css-750x450.jpg);
+        background-attachment: fixed;
+        background-position: center;
+        background-size: cover;
+        height: 700px;
+    }
+
+    #welcome-text {
+        text-align: center;
+    }
+
+    .block {
+        background: rgba(128, 128, 128, 128);
+        border: #FFF 20px double;
+        border-radius: 10px;
+        color: #FFG;
+        display: inline-block;
+        font-size: 60px;
+        font-weight: 800;
+        padding: 0 10px 7px;
+        margin: 250px;
+        text-align: center;
+    }
+
+
+    /* End of Welcome Page */
+
+    #aboutme {
+        background-size: cover;
+        background: #d3d9d9;
+        background-image: url(https://i.stack.imgur.com/jGlzr.png);
+        background-attachment: fixed;
+        background-position: center;
+        height: 700px;
+    }
+
+    #portfolio {
+        background-size: cover;
+        background-image: url(https://images6.alphacoders.com/389/389875.jpg);
+        background-attachment: fixed;
+        background-position: center;
+        height: 700px;
+    }
+
+    #footer {
+        text-align: center;
+        background-size: cover;
+        background-image: url(http://carmel.coop/wp-content/uploads/2014/04/Blurred-Background_4.jpg);
+        background-attachment: fixed;
+        background-position: center;
+        height: 250px;
+        padding: 20px;
+    }
+
+    .fs {
+        font-size: 3em;
+    }
+
+    .img {
+        margin: 60px 20px;
+        border-radius: 50%;
+        height: 550px;
+        display: inline-block;
+        transform: scale(-1, 1);
+        -webkit-transform: scale(-1, 1);
+        -moz-transform: scale(-1, 1);
+        -o-transform: scale(-1, 1);
+    }
+
+    .title {
+        display: inline-block;
+        background-color: grey;
+        border: #AAA 5px double;
+        border-radius: 10px;
+        color: blue;
+        padding-left: 40px;
+        padding-right: 40px;
+        text-shadow: 2px 2px 5px #000;
+        font-size: 30px;
+    }
+
+    .icon-btn {
+        margin: 0 20px;
+    }
+</style>
+
+<body>
+    <svg width="380px" height="500px" viewBox="0 0 837 1045" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+            <path d="M353,9 L626.664028,170 L626.664028,487 L353,642 L79.3359724,487 L79.3359724,170 L353,9 Z" id="Polygon-1" stroke="#007FB2" stroke-width="6" sketch:type="MSShapeGroup"></path>
+            <path d="M78.5,529 L147,569.186414 L147,648.311216 L78.5,687 L10,648.311216 L10,569.186414 L78.5,529 Z" id="Polygon-2" stroke="#EF4A5B" stroke-width="6" sketch:type="MSShapeGroup"></path>
+            <path d="M773,186 L827,217.538705 L827,279.636651 L773,310 L719,279.636651 L719,217.538705 L773,186 Z" id="Polygon-3" stroke="#795D9C" stroke-width="6" sketch:type="MSShapeGroup"></path>
+            <path d="M639,529 L773,607.846761 L773,763.091627 L639,839 L505,763.091627 L505,607.846761 L639,529 Z" id="Polygon-4" stroke="#F2773F" stroke-width="6" sketch:type="MSShapeGroup"></path>
+            <path d="M281,801 L383,861.025276 L383,979.21169 L281,1037 L179,979.21169 L179,861.025276 L281,801 Z" id="Polygon-5" stroke="#36B455" stroke-width="6" sketch:type="MSShapeGroup"></path>
+        </g>
+    </svg>
+    <div class="message-box">
+        <h1>Welcome</h1>
+        <p>Sample page</p>
+        <div class="buttons-con">
+            <div class="action-link-wrap">
+                <a onclick="history.back(-1)" class="link-button link-back-button">Go Back</a>
+                <a href="/home" class="link-button">Go to Home Page</a>
+            </div>
+        </div>
+    </div>
+
+  </body>
+
+  </html>
